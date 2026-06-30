c007c5cf3fe8ddec382439dea6d0f51a94988b25
John Cronin <jncronin@tysos.org>
2026-06-30 17:33:56 +0100

    add gkjoy script

---
+++ 01_base/gkjoy/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+source ../../scripts/check.sh
+mkdir -p ${REPOROOT}/base
+
+cmake $CMAKE_OPTS -S ../../src/gk-userland/gk-joycalib -B build-gkrepo
+make -j16 -C build-gkrepo
+cd build-gkrepo && cpack && cp gkjoy-0.1.1-gk.tar.gz ${REPOROOT}/base && cd ../..
