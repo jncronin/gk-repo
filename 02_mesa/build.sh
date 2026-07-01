d9110d4b15a4e3f394d1db629f57018394638731
John Cronin <jncronin@tysos.org>
2026-07-01 12:46:22 +0100

    copy missing drm headers across

--- 02_mesa/build.sh
+++ 02_mesa/build.sh
@@ -10,6 +10,11 @@ meson setup --cross-file ../src/gk-userland/meson_cross_file.txt -Dintel=disable
 meson compile -C build-gkrepo/libdrm
 meson install -C build-gkrepo/libdrm
 
+# drm.h not installed by the above - fix
+cp ../src/gk-userland/libdrm-2.4.131/include/drm/drm.h $SYSROOT/usr/include
+cp ../src/gk-userland/libdrm-2.4.131/include/drm/drm_mode.h $SYSROOT/usr/include
+cp ../src/gk-userland/libdrm-2.4.131/include/drm/drm_fourcc.h $SYSROOT/usr/include
+
 # build mesa
 meson setup -Dgallium-drivers=etnaviv -Dvulkan-drivers= -Dplatforms= -Degl=enabled -Dglx=disabled -Dglx-direct=false -Ddefault_library=static --cross-file ../src/gk-userland/meson_cross_file.txt -Dbuildtype=release -Db_ndebug=true -Dcmake_prefix_path=~/opt/gkv4/usr/lib/cmake -Dshader-cache=enabled -Dshader-cache-default=true -Ddri-drivers-path=lib --prefix $SYSROOT/usr build-gkrepo/mesa ../src/mesa
 
