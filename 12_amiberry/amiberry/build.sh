743d0ec1297d26a9127e5bb97d79bb42682db2b3
John Cronin <jncronin@tysos.org>
2026-07-02 14:25:27 +0100

    fix incorrect install directory

--- 12_amiberry/amiberry/build.sh
+++ 12_amiberry/amiberry/build.sh
@@ -7,4 +7,4 @@ mkdir -p ${REPOROOT}/amiberry
 
 cmake $CMAKE_OPTS -S ../../src/amiberry-lite -B build-gkrepo -DUSE_PCEM=OFF -DUSE_LIBSERIALPORT=OFF -DUSE_LIBENET=OFF -DUSE_PORTMIDI=OFF -DUSE_LIBMPEG2=OFF -DUSE_ZSTD=OFF
 make -j16 -C build-gkrepo
-cd build-gkrepo && cpack && cp amiberry-lite-5.9.2-gk.tar.gz ${REPOROOT}/mupen64 && cd ..
+cd build-gkrepo && cpack && cp amiberry-lite-5.9.2-gk.tar.gz ${REPOROOT}/amiberry && cd ..
