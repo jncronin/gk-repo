8213a302dd57ace455aec10ce842adc46e6eb12c
John Cronin <jncronin@tysos.org>
2026-07-15 11:28:34 +0100

    add binary stripping to etr build

--- 20_etr/etr/build.sh
+++ 20_etr/etr/build.sh
@@ -14,5 +14,6 @@ mkdir -p build-gkrepo
 pushd build-gkrepo >> /dev/null
 mkdir -p etr-gk/bin
 cp -dpR ../../../src/extremetuxracer/etr etr-gk/bin
+aarch64-none-gkos-strip -S etr-gk/bin/etr
 tar zcf ${REPOROOT}/etr/etr-gk.tar.gz etr-gk
 popd >> /dev/null
