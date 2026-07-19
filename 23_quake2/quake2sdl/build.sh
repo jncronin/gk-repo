eaa21a81bf5b5e544eaf3ae3d561895be733c5f3
John Cronin <jncronin@tysos.org>
2026-07-19 12:50:23 +0100

    disable candy_gl (causes slowdowns in railgun firing)

--- 23_quake2/quake2sdl/build.sh
+++ 23_quake2/quake2sdl/build.sh
@@ -5,6 +5,6 @@ set -e
 source ../../scripts/check.sh
 mkdir -p ${REPOROOT}/quake2
 
-cmake $CMAKE_OPTS -S ../../src/quake2sdl -B build-gkrepo -DQGL=ON -DWITH_QMAX=ON
+cmake $CMAKE_OPTS -S ../../src/quake2sdl -B build-gkrepo -DQGL=ON -DWITH_QMAX=OFF
 make -j16 -C build-gkrepo
 cd build-gkrepo && cpack && cp "quake2sdl-0.17-gk.tar.gz" ${REPOROOT}/quake2 && cd ..
